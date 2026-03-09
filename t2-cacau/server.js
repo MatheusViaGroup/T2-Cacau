20e604e07b79137a72da33122e84a9e6b862c3d0
MatheusViaGroup <matheus.henrique@viagroup.com.br>
2026-03-09 08:11:51 -0300

    Delete t2-cacau directory

--- t2-cacau/server.js
+++
@@ -1,3 +0,0 @@
-// ESTE ARQUIVO FOI DESCONTINUADO
-// O sistema agora roda 100% no navegador (Client-Side) utilizando LocalStorage e dados Mock.
-// Não é necessário rodar "node server.js". Use apenas "npm run dev".
